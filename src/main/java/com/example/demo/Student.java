a0a1681664fd9b4c4c6d2da334de21889fcf4846
donura24 <teodorpenchev24@gmail.com>
2023-03-16 22:16:47 +0200

    Reg commit

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,7 +1,22 @@
 package com.example.demo;
 
+import jdk.jfr.Enabled;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstname;
     private String lastName;
