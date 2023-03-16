a445c96c5340633423c62dc9e1b0c26ffd3f6fda
donura24 <teodorpenchev24@gmail.com>
2023-03-16 22:01:34 +0200

    Reg commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+public class Student {
+
+    private Long id;
+    private String firstname;
+    private String lastName;
+    private String email;
+    private Integer age;
+
+    public Student(Long id,
+                   String firstname,
+                   String lastName,
+                   String email,
+                   Integer age) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
