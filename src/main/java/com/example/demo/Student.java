54ad396f9053e36aea600e11f0ea88117be1c640
donura24 <teodorpenchev24@gmail.com>
2023-03-16 22:33:25 +0200

    Reg commit

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -5,6 +5,10 @@ import jdk.jfr.Enabled;
 import javax.persistence.*;
 
 @Entity(name = "Student")
+@Table(name = "student",
+uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique",columnNames = "email")
+})
 public class Student {
 
     @Id
@@ -17,10 +21,22 @@ public class Student {
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
     )
+    @Column(name = "id", updatable = false)
     private Long id;
-    private String firstname;
+    @Column(name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT")
+    private String firstName;
+    @Column(name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT")
     private String lastName;
+    @Column(name = "email",
+            nullable = false,
+            columnDefinition = "TEXT")
     private String email;
+    @Column(name = "age",
+            nullable = false)
     private Integer age;
 
     public Student(Long id,
@@ -29,10 +45,14 @@ public class Student {
                    String email,
                    Integer age) {
         this.id = id;
-        this.firstname = firstname;
+        this.firstName = firstname;
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
@@ -44,11 +64,11 @@ public class Student {
     }
 
     public String getFirstname() {
-        return firstname;
+        return firstName;
     }
 
     public void setFirstname(String firstname) {
-        this.firstname = firstname;
+        this.firstName = firstname;
     }
 
     public String getLastName() {
@@ -79,7 +99,7 @@ public class Student {
     public String toString() {
         return "Student{" +
                 "id=" + id +
-                ", firstname='" + firstname + '\'' +
+                ", firstname='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", age=" + age +
